690dfef2d1d35c01cd6f7bdc7c894f29742fbbaf
avat_ov <narimah9s9@gmail.com>
2021-09-10 15:12:31 +0300

    initial

---
+++ MultiplicationTable.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(MultiplicationTable(3)[:])
+}
+
+func MultiplicationTable(n int) [][]int {
+	return InitArray(CreateArray(n))
+}
+
+func CreateArray(size int) [][]int {
+	array := make([][]int, size)
+	for i := range array {
+		array[i] = make([]int, size)
+	}
+	return array
+}
+
+func InitArray(array [][]int) [][]int {
+	for i := range array {
+		for j := range array {
+			array[i][j] = (j + 1) * (i + 1)
+		}
+	}
+	return array
+}
---
+++ Pyramid.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(InitPyro(MakePyro(1)))
+}
+
+func MakePyro(n int) [][]uint {
+	pyro := make([][]uint, n)
+
+	for i := range pyro {
+		pyro[i] = make([]uint, i+1)
+	}
+
+	return pyro
+}
+
+func InitPyro(pyro [][]uint) [][]uint {
+	for i := range pyro {
+		for j := range pyro[i] {
+			pyro[i][j] = 1
+		}
+	}
+	return pyro
+}
